b4217e3b6690ec02913a53364d2a68731dbe617b
Christian Bundy <christianbundy@fraction.io>
2020-11-20 18:06:44 -0800

    Make sync-upstream pretty

--- .local/src/sync-upstream.sh
+++ .local/src/sync-upstream.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-remote="$(git remote show | head -n 1)"; git fetch "$remote"; branch="$(git remote show "$remote" | grep 'HEAD branch:' | awk '{ print $3 }')"; git checkout "$branch"; git pull "$remote" "$branch"
+remote="$(git remote show | head -n 1)"
+git fetch "$remote"
+
+branch="$(git remote show "$remote" | grep 'HEAD branch:' | awk '{ print $3 }')"
+git checkout "$branch"
+
+git pull "$remote" "$branch"
