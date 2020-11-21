b4165deee19774a36ec5282119b5ca0e5957bc7b
Christian Bundy <christianbundy@fraction.io>
2020-11-20 18:08:52 -0800

    Add quick docs to sync-upstream

--- .local/src/sync-upstream.sh
+++ .local/src/sync-upstream.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# 'git pull origin master' is cool, but what if you're on a detached head?
+# what if you don't know if the repo uses 'origin'?
+# what if it doesn't use 'master'?
+#
+# git knows your remote and branch name, let it help!
+
 remote="$(git remote show | head -n 1)"
 git fetch "$remote"
 
