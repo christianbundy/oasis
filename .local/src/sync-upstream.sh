19e1a87925d9974a1e74bcd6b5d4e6a0f45bbf1e
Christian Bundy <christianbundy@fraction.io>
2020-11-20 18:05:46 -0800

    Add sync-upstream

---
+++ .local/src/sync-upstream.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+remote="$(git remote show | head -n 1)"; git fetch "$remote"; branch="$(git remote show "$remote" | grep 'HEAD branch:' | awk '{ print $3 }')"; git checkout "$branch"; git pull "$remote" "$branch"
