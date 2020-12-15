6c7cdbd56d7c919c52af5c0bc3e92fc22dd2fdf2
Christian Bundy <christian@truework.com>
2020-12-15 09:31:23 -0800

    Remove Homebrew build-from-source command

--- .local/src/brew.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ "$1" = install ] || [ "$1" = upgrade ]; then
-  brew_command="$1"
-  shift # remove brew_command from $@
-  original brew "$brew_command" --build-from-source "$@"
-else
-  original brew "$@"
-fi
